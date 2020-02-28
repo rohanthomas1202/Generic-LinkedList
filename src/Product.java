d606c823b10e6fbe8bdad70e7065093f7601fd68
rohanthomas1202 <rst170000@utdallas.edu>
2020-02-28 13:09:13 -0600

    updated product and main

--- src/Product.java
+++ src/Product.java
@@ -7,13 +7,58 @@ public class Product implements IDedObject{
     private String supplierName;
 
 
+
+    // overriding function getID() from interface IDed Object
+    // function returns the ID of the current product
     @Override
     public int getID() {
         return productID;
     }
 
+    // constructor for Product
+    public Product(int productID, String productName, String supplierName) {
+        this.productID = productID;
+        this.productName = productName;
+        this.supplierName = supplierName;
+    }
+
+    // overriding the function printID() from interface IDedObject
+    // function prints out details about the product
     @Override
     public void printID() {
-
+        System.out.println("Product ID   \t: " + productID);
+        System.out.println("Product Name \t: " + productName);
+        System.out.println("Supplier name\t: " + supplierName);
     }
+
+
+    ///////////////////////// Getters and Setters for  variables /////////////////////////
+
+    public int getProductID() {
+        return productID;
+    }
+
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    ///////////////////////// Getters and Setters for  variables /////////////////////////
+
 }
---
+++ src/main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+        int product_ID;
+        String product_name;
+        String product_supplier;
+
+        System.out.println("Enter product_ID");
+        product_ID = Integer.parseInt(sc.nextLine());
+        System.out.println("Enter product_name");
+        product_name = sc.nextLine();
+        System.out.println("Enter product_supplier");
+        product_supplier = sc.nextLine();
+
+
+        Product p = new Product(product_ID,product_name,product_supplier);
+
+        p.printID();
+
+    }
+}
