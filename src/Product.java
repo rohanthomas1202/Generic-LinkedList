1958e4e578c9d4be02d856a8f2b722e0adc5ba76
rohanthomas1202 <rst170000@utdallas.edu>
2020-02-28 11:51:35 -0600

    Updated readmefile

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,12 @@
-public class Product {
+public class Product implements IDedObject{
+
+    @Override
+    public int getID() {
+        return 0;
+    }
+
+    @Override
+    public void printID() {
+
+    }
 }
