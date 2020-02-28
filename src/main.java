183038812ab191aaef3f397a7bcb214b4e0507ff
rohanthomas1202 <rst170000@utdallas.edu>
2020-02-28 16:54:24 -0600

    Implemented menu system

--- src/main.java
+++ src/main.java
@@ -6,6 +6,7 @@
  ** JDK     - 11                                                    **
  ** IDE     - IntelliJ Idea                                         **
  *********************************************************************/
+
 import java.util.Scanner;
 
 public class main {
@@ -15,57 +16,55 @@ public class main {
         Scanner sc = new Scanner(System.in);
 
 
-        int product_ID = 0;
-        String product_name = "roku";
-        String product_supplier = "Amz";
-/*
-        System.out.println("Enter product_ID");
-        product_ID = Integer.parseInt(sc.nextLine());
-        System.out.println("Enter product_name");
-        product_name = sc.nextLine();
-        System.out.println("Enter product_supplier");
-        product_supplier = sc.nextLine();
-*/
-
-/* TEST*/
-        boolean a;
-        boolean s;
-        boolean d;
-        boolean f;
-        boolean g;
-        boolean h;
+        int user_val = -1;
+        do {
 
 
-        Product q = new Product(product_ID + 1, product_name, product_supplier);
-        Product w = new Product(product_ID + 2, product_name, product_supplier);
-        Product e = new Product(product_ID + 3, product_name, product_supplier);
-        Product r = new Product(product_ID + 4, product_name, product_supplier);
-        Product t = new Product(product_ID + 5, product_name, product_supplier);
-        Product y = new Product(product_ID + 6, product_name, product_supplier);
-
-        Product_Inventory<Product> inventory = new Product_Inventory<>();
-        a = inventory.insertAtFront(q);
-        s = inventory.insertAtFront(w);
-        d = inventory.insertAtFront(e);
-        f = inventory.insertAtFront(r);
-        g = inventory.insertAtFront(t);
-        h = inventory.insertAtFront(y);
-
-        System.out.println("can add " + q.getID() + " ? : " + a);
-        System.out.println("can add " + w.getID() + " ? : " + s);
-        System.out.println("can add " + e.getID() + " ? : " + d);
-        System.out.println("can add " + r.getID() + " ? : " + f);
-        System.out.println("can add " + t.getID() + " ? : " + g);
-        System.out.println("can add " + y.getID() + " ? : " + h);
-
-        inventory.delete(5);
-        inventory.deleteFromFront();
-
-        inventory.printAllRecords();
-
-        inventory.makeEmpty();
+            print_menu();
+            System.out.print("Your choice: ");
+            user_val = Integer.parseInt(sc.nextLine());
 
 
+            switch (user_val) {
+                case 1:
+                    System.out.println("Your choice: " + user_val);
+                    break;
+                case 2:
+                    System.out.println("Your choice: " + user_val);
+                    break;
+                case 3:
+                    System.out.println("Your choice: " + user_val);
+                    break;
+                case 4:
+                    System.out.println("Your choice: " + user_val);
+                    break;
+                case 5:
+                    System.out.println("Your choice: " + user_val);
+                    break;
+                case 6:
+                    System.out.println("Your choice: " + user_val);
+                    break;
+                case 7:
+                    System.out.println("Your choice: " + user_val);
+                    break;
+                default:
+                    System.out.println("Select a valid choice(1 to 6)");
+                    break;
+            }
 
+
+        } while (user_val != 7);
     }
+
+    public static void print_menu(){
+        System.out.println("Operations on List");
+        System.out.println("\t1. Make Empty");
+        System.out.println("\t2. Find ID");
+        System.out.println("\t3. Insert At Front");
+        System.out.println("\t4. Delete From Front");
+        System.out.println("\t5. Delete ID");
+        System.out.println("\t6. Print all Records");
+        System.out.println("\t7. Done");
+    }
+
 }
